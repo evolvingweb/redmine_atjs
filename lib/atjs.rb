44f6410d94be9e0e054a53a88a7d5dcab9f5bfeb
Alex Dergachev <alex@evolvingweb.ca>
2013-10-18 14:46:43 -0400

    Ensured css/jss only get loaded on wiki and issue controllers

--- lib/atjs.rb
+++ lib/atjs.rb
@@ -2,29 +2,25 @@ require_dependency 'application_helper'
 
 class AtJsHook < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context={})
+    return unless ["WikiController", "IssuesController"].include?(context[:controller].class.name)
 
-      js_files = %w{jquery.min.js jquery.atwho.js jquery.noconflict.js redmine-atjs.js}
-
-      js_files.map do |file| 
-        javascript_include_tag(file, :plugin=> 'redmine_atjs')
-      end
-
-  #   controller = context[:controller].class.name
-  #   action = context[:request].parameters[:action] 
-
-  #   if ["WikiController", "IssuesController"].include?(controller) && action == "edit"
-  #   end
+    js_files = %w{jquery.min.js jquery.atwho.js jquery.noconflict.js redmine-atjs.js}
+    js_files.map do |file| 
+      javascript_include_tag(file, :plugin=> 'redmine_atjs')
+    end
   end
 end
 
+# for some reason merging these hooks together causes only the CSS to appear; not JS
 class AtJsCSSHook < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context={})
+    return unless ["WikiController", "IssuesController"].include?(context[:controller].class.name)
 
-      css_files = %w{jquery.atwho.css}
+    css_files = %w{jquery.atwho.css}
 
-      css_files.map do |file| 
-        stylesheet_link_tag(file, :plugin=> 'redmine_atjs')
-      end
+    css_files.map do |file| 
+      stylesheet_link_tag(file, :plugin=> 'redmine_atjs')
+    end
   end
 end
 
